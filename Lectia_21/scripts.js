f269d847e5d056796d0a6b37df7a7092fbd52627
Claudiu Radu <claudiu@projects-abroad.net>
2018-02-23 17:11:00 +0200

    lectia 21
    lectia 21

---
+++ Lectia_21/scripts.js
@@ -0,0 +1,53 @@
+$(() => {
+    setToggleEvent();
+    setExpandEvent();
+});
+
+const setToggleEvent = () => {
+    $('.title').click(toggleDescription);
+};
+
+
+function toggleDescription() {
+    const titleIcon = $(this).find('.fas');
+    const description = $(this).next('.description');
+
+    if (description.hasClass('opened')) {
+        description.removeClass('opened');
+        description.toggle(1000);
+        toggleElement(titleIcon);
+        return;
+    }
+
+    const icon = $('.opened')
+        .prev('.title')
+        .find('.fas');
+    toggleElement(icon);
+
+    $('.opened').toggle(1000);
+    $('.opened').removeClass('opened');
+
+    description.addClass('opened');
+    description.toggle(1000);
+    toggleElement(titleIcon);
+}
+
+const toggleElement = element => {
+    element.toggleClass('fa-chevron-down fa-chevron-up');
+};
+
+
+
+
+const setExpandEvent = () => {
+    $('.expand').click(toggleExpand);
+};
+
+function toggleExpand() {
+    $('.description').not('.opened').each(function () {
+        $(this).addClass('opened');
+        $(this).toggle(1000);
+    });
+
+}
+
